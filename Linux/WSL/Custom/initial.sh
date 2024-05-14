ab9332f324bd807057ad695b97b09699944cd7a4
Ihan Messias N. Dos Santos <72288211+ihanmessias@users.noreply.github.com>
2024-05-14 21:03:05 +0100

    Update initial.sh

--- Linux/WSL/Custom/initial.sh
+++ Linux/WSL/Custom/initial.sh
@@ -4,7 +4,7 @@
 sudo apt update && sudo apt upgrade -y
 
 # ---- Instalando pacotes ---- #
-sudo apt install git curl gnome-tweaks texlive texlive-latex-extra pandoc zsh dconf-cli -y
+sudo apt install git curl gnome-tweaks texlive texlive-latex-extra texlive-xetex texlive-fonts-recommended texlive-plain-generic pandoc zsh dconf-cli -y 
 # ---- Baixando OhMyZSH ---- #
 sleep 2
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
